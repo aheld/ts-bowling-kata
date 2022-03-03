1dbc12f68c526372db5e664d78f0dc7acfa8b19d
aheld <aaron.held@gopuff.com>
2022-03-02 22:50:10 -0500

    jest test time reduced from 4 sec to 1 sec, still too slow but tolerable now

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@
 /* eslint-disable */
 module.exports = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  maxWorkers: '1',
 }
