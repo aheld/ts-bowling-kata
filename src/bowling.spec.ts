a5daa37ae385cd00e0dd355e986ef952391da2b9
aheld <aaron.held@gopuff.com>
2022-03-03 22:31:52 -0500

    working 1

--- src/bowling.spec.ts
+++ src/bowling.spec.ts
@@ -1,5 +1,11 @@
 import { Game } from './bowling'
 
+function rollMany(game: Game, times: number, pins: number) {
+  for (let i = 0; i < times; i++) {
+    game.roll(pins)
+  }
+}
+
 describe('Basic Game', () => {
   let game: Game
 
@@ -8,7 +14,74 @@ describe('Basic Game', () => {
   })
 
   test('Gutter Game (all zeros)', () => {
-    for (let i = 0; i < 20; i++) game.roll(0)
+    rollMany(game, 20, 0)
     expect(game.score()).toBe(0)
+  })
+
+  test('Simple Game', () => {
+    rollMany(game, 20, 1)
+    expect(game.score()).toBe(20)
+  })
+
+  const testcases = [
+    [0, 0, 0],
+    [3, 4, 70],
+    [4, 1, 50],
+  ]
+  test.each(testcases)(
+    'Roll 10 %i, 10 %i score %i',
+    (rollA, rollB, expected) => {
+      rollMany(game, 10, rollA)
+      rollMany(game, 10, rollB)
+      expect(game.score()).toBe(expected)
+    }
+  )
+})
+
+describe('Advanced Game', () => {
+  let game: Game
+
+  beforeEach(() => {
+    game = new Game()
+  })
+
+  test('Spare Game', () => {
+    rollMany(game, 3, 5)
+    rollMany(game, 17, 0)
+    expect(game.score()).toBe(20)
+  })
+
+  test('Strike Game', () => {
+    rollMany(game, 1, 10)
+    rollMany(game, 2, 3)
+    rollMany(game, 16, 0)
+    expect(game.score()).toBe(22)
+  })
+
+  test('Last Frame Strike Game', () => {
+    rollMany(game, 18, 0)
+    rollMany(game, 1, 10)
+    rollMany(game, 2, 4)
+    expect(game.score()).toBe(18)
+  })
+
+  test('Last Frame Spare Game', () => {
+    rollMany(game, 18, 0)
+    rollMany(game, 3, 5)
+    expect(game.score()).toBe(15)
+  })
+
+  const moreTests: Array<Array<number[] | number>> = [
+    [[5, 0, 3, 4, 5, 2, 9, 0, 3, 3, 6, 3, 7, 3, 9, 0, 3, 4, 9, 0], 87],
+    [[10, 3, 3], 22],
+    [[3, 4, 6, 4, 7, 1, 4, 5, 10, 4, 6, 10, 10, 10, 5, 5, 3], 169],
+  ]
+  test.each(moreTests)('bulk tests %s, %s', (rolls, score) => {
+    if (typeof rolls === 'number') rolls = [rolls]
+    rolls.forEach(roll => {
+      game.roll(roll)
+    })
+    const res = game.score()
+    expect(res).toBe(score)
   })
 })
--- src/bowling.ts
+++ src/bowling.ts
@@ -1,9 +1,101 @@
-export class Game {
+export class Frame {
+  _roll1: number | undefined
+  _roll2: number | undefined
+
+  get roll1(): number {
+    return this._roll1 || 0
+  }
+
+  get roll2(): number {
+    return this._roll2 || 0
+  }
+
+  get rolls(): number[] {
+    return [this._roll1, this._roll2].filter(r => typeof r === 'number')
+  }
+
   roll(roll: number) {
-    console.log(roll) //put here to make the linter happy
+    if (this._roll1 !== undefined) this._roll2 = roll
+    else this._roll1 = roll
+  }
+
+  complete(): boolean {
+    if (this.isStrike()) return true
+    return this._roll1 !== undefined && this._roll2 !== undefined
+  }
+
+  isSpare(): boolean {
+    return !this.isStrike() && this.roll1 + this.roll2 === 10
+  }
+
+  isStrike(): boolean {
+    return this.roll1 === 10
+  }
+}
+
+class TenthFrame extends Frame {
+  _roll3: number | undefined
+
+  get roll3(): number {
+    return this._roll3 || 0
+  }
+
+  get rolls(): number[] {
+    return [this._roll1, this._roll2, this._roll3].filter(r => r !== undefined)
+  }
+
+  roll(roll: number) {
+    if (this._roll1 === undefined) {
+      this._roll1 = roll
+    } else if (this._roll2 === undefined) {
+      this._roll2 = roll
+    } else {
+      this._roll3 = roll
+    }
+  }
+
+  complete(): boolean {
+    return false
+  }
+}
+
+export class Game {
+  frames: (Frame | TenthFrame)[] = new Array(10)
+  currentFrame: number
+
+  constructor() {
+    for (let i = 0; i < 9; i++) {
+      this.frames[i] = new Frame()
+    }
+    this.frames[9] = new TenthFrame()
+    this.currentFrame = 0
+  }
+
+  roll(roll: number) {
+    if (this.frames[this.currentFrame].complete()) {
+      this.currentFrame++
+    }
+    this.frames[this.currentFrame].roll(roll)
   }
 
   score(): number {
-    return -1
+    let _score = 0
+    for (let i = 0; i < this.frames.length; i++) {
+      const frame = this.frames[i]
+      _score += frame.roll1 + frame.roll2
+      if (frame instanceof TenthFrame) {
+        _score += frame.roll3
+      } else {
+        if (frame.isSpare()) _score += this.frames[i + 1].roll1
+        if (frame.isStrike()) {
+          let nextRolls = this.frames[i + 1].rolls
+          if (nextRolls.length < 2) {
+            nextRolls = nextRolls.concat(this.frames[i + 2].rolls)
+          }
+          _score += nextRolls[0] + nextRolls[1]
+        }
+      }
+    }
+    return _score
   }
 }
